15ac823730c01169c3486212b5fafb087d027389
Beatriz Bittencourt <beatrizbittencourtdealmeida@gmail.com>
2024-11-02 21:41:41 -0300

    express adicionado no server

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -23,7 +23,7 @@ export const ChatContextProvider = ({ children, user }) => {
   console.log("notifications", notifications);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:3000");
+    const newSocket = io(import.meta.env.VITE_SOCKET_URL);
     setSocket(newSocket);
 
     return () => {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const { Server } = require("socket.io");
+
 const userRoute = require("./Routes/userRoute");
 const chatRoute = require("./Routes/chatRoute");
 const messageRoute = require("./Routes/messageRoute");
@@ -21,7 +23,7 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
 
-app.listen(port, (req, res) => {
+const expressServer = app.listen(port, (req, res) => {
   console.log(`Server running on port: ${port}`);
 });
 
@@ -32,3 +34,48 @@ mongoose
   })
   .then(() => console.log("MongoDB connection established"))
   .catch((error) => console.log("MongoDB connection failed: ", error.message));
+
+const io = new Server(expressServer, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+
+  socket.on("addNewUser", (userId) => {
+    !onlineUsers.some((user) => user.userId === userId) &&
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      });
+    console.log("onlineUsers", onlineUsers);
+
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+
+  socket.on("sendMessage", (message) => {
+    const user = onlineUsers.find(
+      (user) => user.userId === message.recipientId
+    );
+
+    if (user) {
+      io.to(user.socketId).emit("getMessage", message);
+      io.to(user.socketId).emit("getNotification", {
+        senderId: message.senderId,
+        isRead: false,
+        date: new Date(),
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+});
